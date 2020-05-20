ee757d3749cedac67aaf6fb3908622c3008e1bc5
Ashkan Abbasi <airashkan@yahoo.com>
2020-05-19 20:01:17 -0600

    Write tests for user story 30 31.

--- spec/features/pets/index_spec.rb
+++ spec/features/pets/index_spec.rb
@@ -4,8 +4,8 @@ describe "Pets Index Page" do
   describe "When I visit '/pets'" do
     it "Can see each pet in the system" do
       shelter1 = Shelter.create(name: "Puppies United")
-      pet1 = Pet.create(image: "https://pixabay.com/get/54e7dd464a55a814f1dc8460da2932761d37d6e1555874_640.jpg", name: "Rosco", approximate_age: "5", sex: "Male", shelter_id: shelter1.id)
-      pet2 = Pet.create(image: "https://pixabay.com/get/5ee0d44b4854b10ff3d89960c62d3f761d37dae25757_640.jpg", name: "Bob", approximate_age: "1", sex: "Male", shelter_id: shelter1.id)
+      pet1 = Pet.create(image: "https://pixabay.com/get/54e7dd464a55a814f1dc8460da2932761d37d6e1555874_640.jpg", name: "Rosco", approximate_age: "5", sex: "Male", description: "It's a pup", shelter_id: shelter1.id)
+      pet2 = Pet.create(image: "https://pixabay.com/get/5ee0d44b4854b10ff3d89960c62d3f761d37dae25757_640.jpg", name: "Bob", approximate_age: "1", sex: "Male", description: "It's a pup", shelter_id: shelter1.id)
 
       visit "/pets"
 
@@ -24,7 +24,7 @@ describe "Pets Index Page" do
 
     it "can update a pet's info" do
       shelter1 = Shelter.create(name: "Puppies United")
-      pet1 = Pet.create(image: "https://pixabay.com/get/54e7dd464a55a814f1dc8460da2932761d37d6e1555874_640.jpg", name: "Rosco", approximate_age: "5", sex: "Male", shelter_id: shelter1.id)
+      pet1 = Pet.create(image: "https://pixabay.com/get/54e7dd464a55a814f1dc8460da2932761d37d6e1555874_640.jpg", name: "Rosco", approximate_age: "5", sex: "Male", description: "It's a pup", shelter_id: shelter1.id)
 
       visit "/pets"
 
@@ -36,9 +36,9 @@ describe "Pets Index Page" do
     it "can delete a pet" do
       shelter1 = Shelter.create(name: "Puppies United")
       shelter2 = Shelter.create(name: "Kittens R Us")
-      pet1 = Pet.create(image: "https://pixabay.com/get/54e7dd464a55a814f1dc8460da2932761d37d6e1555874_640.jpg", name: "Rosco", approximate_age: "5", sex: "Male", shelter_id: shelter1.id)
-      pet2 = Pet.create(image: "https://pixabay.com/get/5ee0d44b4854b10ff3d89960c62d3f761d37dae25757_640.jpg", name: "Bob", approximate_age: "1", sex: "Male", shelter_id: shelter1.id)
-      pet3 = Pet.create(image: "https://pixabay.com/get/5ee0d44b4854b10ff3d89960c62d3f761d37dae25757_640.jpg", name: "Lars", approximate_age: "14", sex: "Male", shelter_id: shelter2.id)
+      pet1 = Pet.create(image: "https://pixabay.com/get/54e7dd464a55a814f1dc8460da2932761d37d6e1555874_640.jpg", name: "Rosco", approximate_age: "5", sex: "Male", description: "It's a pup", shelter_id: shelter1.id)
+      pet2 = Pet.create(image: "https://pixabay.com/get/5ee0d44b4854b10ff3d89960c62d3f761d37dae25757_640.jpg", name: "Bob", approximate_age: "1", sex: "Male", description: "It's a pup", shelter_id: shelter1.id)
+      pet3 = Pet.create(image: "https://pixabay.com/get/5ee0d44b4854b10ff3d89960c62d3f761d37dae25757_640.jpg", name: "Lars", approximate_age: "14", sex: "Male", description: "It's a pup", shelter_id: shelter2.id)
 
       visit "/pets"
 
@@ -48,6 +48,27 @@ describe "Pets Index Page" do
       expect(page).to have_content(pet2.name)
       expect(page).to have_content(pet3.name)
       expect(page).to_not have_content(pet1.name)
+    end
+
+    it "Can not delete a pet if the pet has a approved application" do
+      shelter1 = Shelter.create(name: "Puppies United")
+      pet1 = Pet.create(image: "https://pixabay.com/get/54e7dd464a55a814f1dc8460da2932761d37d6e1555874_640.jpg", name: "Rosco", approximate_age: "5", sex: "Male", description: "It's a pup", shelter_id: shelter1.id)
+      application1 = Application.create(name: "Taylor", address: "1805 Main St.", city: "Superior", state: "CO", zip: "80027", phone: "555-555-5555", description: "I love pets")
+      pet_app1 = PetApplication.create(pet_id: pet1.id, application_id: application1.id)
+
+      visit "/applications/#{application1.id}"
+
+      expect(pet_app1.approved).to eq(false)
+
+      within("#pet-#{pet1.id}") do
+        click_on "Approve Application"
+      end
+
+      visit "/pets"
+
+      within("#pet-#{pet1.id}") do
+        expect(page).to_not have_link("Delete")
+      end
     end
   end
 end
